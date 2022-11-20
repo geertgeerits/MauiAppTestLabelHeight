16091306ef336d744fb80051c51752a6f552e927
Geert Geerits <ggeerits@hotmail.com>
2022-11-20 15:22:53 +0100

    Add project files.

---
+++ MauiAppTestLabelHeight/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppTestLabelHeight;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiAppTestLabelHeight/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppTestLabelHeight;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+        lblExplanation1.Text = "Retorno em porcentagens ao ano sobre o investimento:\r\nInsira o valor da compra (+ o valor dos custos) e o valor da receita por ano.\r\n\r\nCalculando o valor do investimento:\r\nInsira o valor do rendimento por ano e o rendimento desejado em porcentagem.";
+        lblExplanation2.Text = "Retorno em porcentagens ao ano sobre o investimento:\r\nInsira o valor da compra (+ o valor dos custos) e o valor da receita por ano.\r\n\r\nCalculando o valor do investimento:\r\nInsira o valor do rendimento por ano e o rendimento desejado em porcentagem.";
+    }
+}
+
